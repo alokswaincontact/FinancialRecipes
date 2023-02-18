46390baa7e72f9b5d8c9a2ccef9f889bda4e096f
Alok Swain <alokswain1@gmail.com>
2023-02-17 23:51:44 -0600

    Reason:     Adding option_price_american_call_approximated_bjerksund_stensland() as     it it called by option_price_american_put_approximated_bjerksund_stensland()     to pass compilation.

---
+++ all_cc_progs/approx_am_call_bjerksund_stensland.cc
@@ -0,0 +1,12 @@
+#include <stdexcept>
+#include "fin_recipes.h"
+
+double option_price_american_call_approximated_bjerksund_stensland( const double& S, 
+								   const double& X, 
+								   const double& r, 
+								   const double& q,
+								   const double& sigma, 
+								   const double& T ){
+     throw std::runtime_error("Not Implemented");
+};
+
